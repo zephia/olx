765c7fc48fcd300243debc02243d0f7f6f71df5a
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2016-11-21 21:30:17 +0000

    TypeError not needed

--- Tests/DocumentTest.php
+++ Tests/DocumentTest.php
@@ -5,19 +5,9 @@ namespace MauroMoreno\AutoLeadDataFormat\Tests;
 use JMS\Serializer\SerializerInterface;
 use Zephia\OLXFeed\Document;
 use Zephia\OLXFeed\Entity\VehicleBag;
-use TypeError;
 
 class DocumentTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException TypeError
-     */
-    public function testGenerateWrongType()
-    {
-        $serializer = $this->mockSerializer();
-        (new Document($serializer))->generate('');
-    }
-
     public function testGenerateOk()
     {
         $serializer = $this->mockSerializer();
